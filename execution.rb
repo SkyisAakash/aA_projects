75c5a86bd98843b642d3fe643daa95741d0ba240
Aakash <sarangaakash@gmail.com>
2018-04-26 17:28:55 -0700

    Finish execution problems

--- execution.rb
+++ execution.rb
@@ -45,7 +45,40 @@ subsets = []
   #Time complexity n^3
 end
 
+def sub_sums_02(array)
+  byebug
+  nums = 0
+  ans = []
+  array.each do
+    |el| ans << el if ans.sum + el > ans.sum
+  end
+  return [array.max] if ans = []
+  ans
+end
 
+def subs(arr)
+  curr_sum = 0
+  max_sum = 0
+  start_idx = 0
+  best_arr = []
+  arr.each_with_index do |el, idx|
+    curr_sum += el
+    case max_sum <=> curr_sum
+    when -1
+      max_sum = curr_sum
+      best_arr = arr[start_idx, idx]
+    when 0
+      next
+    when 1
+      if curr_sum < 0
+        curr_sum = 0
+        start_idx = idx + 1
+      end
+    end
+  end
+  return [arr.max] if max_sum <= 0
+  best_arr
+end
 
 
 
@@ -65,5 +98,7 @@ if __FILE__ == $PROGRAM_NAME
    # p sub_sums(arr)
 
    list = [2, 3, -6, 7, -6, 7]
-  p  sub_sums(list) # => 8 (from [7, -6, 7])
+  p  subs(list) # => 8 (from [7, -6, 7])
+  list = [-5, -1, -3]
+    p subs(list) # => -1 (from [-1])
 end
